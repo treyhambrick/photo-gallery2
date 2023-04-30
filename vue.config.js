80d21d7eaed8c4a9aa49a217b2d51969aa8feb37
treyhambrick <trey.hambrick@gmail.com>
2023-04-30 15:56:40 -0400

    first commit123555

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/photo-gallery/'
+  ? '/photo-gallery/public/index.html'
   : '/'
 })
